279832b69bc14555ea36739910dcb1c735bd6c1b
JingBh <jingbohao@yeah.net>
2024-07-10 15:58:22 +0800

    fix: crash when multiple fractions

--- entry/src/main/java/tech/bjut/appeal/ui/slice/HistorySlice.java
+++ entry/src/main/java/tech/bjut/appeal/ui/slice/HistorySlice.java
@@ -1,6 +1,7 @@
 package tech.bjut.appeal.ui.slice;
 
 import ohos.aafwk.ability.AbilitySlice;
+import ohos.aafwk.ability.fraction.Fraction;
 import ohos.aafwk.ability.fraction.FractionAbility;
 import ohos.aafwk.content.Intent;
 import tech.bjut.appeal.ResourceTable;
@@ -9,20 +10,36 @@ import tech.bjut.appeal.ui.fraction.QuestionsFraction;
 
 public class HistorySlice extends AbilitySlice {
 
+    private Fraction fraction = null;
+
     @Override
     protected void onStart(Intent intent) {
         super.onStart(intent);
         this.setUIContent(ResourceTable.Layout_slice_history);
 
         if (TokenUtil.getToken() == null) {
+            this.fraction = null;
             this.present(new LoginSlice(), new Intent());
             this.terminate();
             return;
         }
 
+        this.fraction = new QuestionsFraction(false, true);
         FractionAbility ability = (FractionAbility) this.getAbility();
         ability.getFractionManager().startFractionScheduler()
-            .replace(ResourceTable.Id_history_content, new QuestionsFraction(false, true))
+            .replace(ResourceTable.Id_history_content, this.fraction)
             .submit();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (this.fraction != null) {
+            FractionAbility ability = (FractionAbility) this.getAbility();
+            ability.getFractionManager().startFractionScheduler()
+                    .remove(this.fraction)
+                    .submit();
+        }
     }
 }
